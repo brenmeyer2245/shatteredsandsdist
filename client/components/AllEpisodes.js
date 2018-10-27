28e492628e859a8475393284caf1dc600d8c6bdb
Brendan Meyer <brenmeyer2245@gmail.com>
2018-10-27 18:04:10 -0500

    Add Search bar bare

--- client/components/AllEpisodes.js
+++ client/components/AllEpisodes.js
@@ -4,11 +4,49 @@ import {connect} from 'react-redux'
 import {fetchEpisodes} from '../store'
 
 class AllEpisodes extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      searchVal: 'Test'
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
   componentDidMount() {
     this.props.getEpisodes()
   }
+
+  handleChange(evt) {
+    console.log(evt.target.value)
+    this.setState({searchVal: evt.target.value})
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault()
+    console.log('YEs!!!!', this.state.searchVal)
+  }
+
   render() {
-    return <Episodes episodes={this.props.episodes} />
+    return (
+      <div>
+        <div>
+          <form
+            className="flex justify-content-end"
+            onSubmit={this.handleSubmit}
+          >
+            <input
+              type="text"
+              name="search"
+              value={this.state.searchVal}
+              onChange={this.handleChange}
+            />
+            <button type="submit"> Search </button>
+          </form>
+        </div>
+        <Episodes episodes={this.props.episodes} />
+      </div>
+    )
   }
 }
 const mapStateToProps = state => ({
