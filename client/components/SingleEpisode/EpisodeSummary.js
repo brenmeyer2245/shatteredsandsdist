4accc6df01f1f2cdcc4ad1a737edf2fb8590668d
Brendan Meyer <bmeyer@BMEYER-012419.local>
2019-03-23 08:42:49 -0500

    Added  Background Scroll to Episode Details

--- client/components/SingleEpisode/EpisodeSummary.js
+++ client/components/SingleEpisode/EpisodeSummary.js
@@ -11,48 +11,50 @@ const EpisodeSummary = props => {
     photos
   } = props
   return (
-    <div className="singleEpisodeDeets mt-4 elevatedCard red-trim flexDown">
-      <h2 className="text-center">
-        {series} Book: {bookNumber}, {bookTitle}
-      </h2>
-      <h2 className="text-center">
-        Chapter {chapterNumber}: {title}
-      </h2>
-      <br />
-      <div>
-        <div className="flex">
-          <div>
-            {photos ? (
-              <img className="episode-summary-img" src={photos[0]} />
-            ) : null}
+    <div className="singleEpisodeDeets-base mt-4 elevatedCard flexDown">
+      <div className="singleEpisodeDeets-contents">
+        <h2 className="text-center">
+          {series} Book: {bookNumber}, {bookTitle}
+        </h2>
+        <h2 className="text-center">
+          Chapter {chapterNumber}: {title}
+        </h2>
+        <br />
+        <div>
+          <div className="flex">
+            <div>
+              {photos ? (
+                <img className="episode-summary-img" src={photos[0]} />
+              ) : null}
+            </div>
+            <div className="episode-summary-text">
+              {summary ||
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+            </div>
           </div>
-          <div className="episode-summary-text">
+          <br />
+          <div className="flex">
+            {summary ||
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+            <div>
+              {photos ? (
+                <img className="episode-summary-img" src={photos[1]} />
+              ) : null}
+            </div>
+          </div>
+          <br />
+          <div className="flex">
+            <div>
+              {photos ? (
+                <img className="episode-summary-img" src={photos[2]} />
+              ) : null}
+            </div>
             {summary ||
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
           </div>
         </div>
         <br />
-        <div className="flex">
-          {summary ||
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
-          <div>
-            {photos ? (
-              <img className="episode-summary-img" src={photos[1]} />
-            ) : null}
-          </div>
         </div>
-        <br />
-        <div className="flex">
-          <div>
-            {photos ? (
-              <img className="episode-summary-img" src={photos[2]} />
-            ) : null}
-          </div>
-          {summary ||
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
-        </div>
-      </div>
-      <br />
     </div>
   )
 }
