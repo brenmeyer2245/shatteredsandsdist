257ae7cc602474791f847f3b1443860700682efd
Brendan Meyer <bmeyer@BMEYER-012419.local>
2019-09-12 20:01:12 -0500

    added extension method

--- client/components/MenuBar/SideBarAudioPlayer.js
+++ client/components/MenuBar/SideBarAudioPlayer.js
@@ -6,13 +6,27 @@ class SideBarAudioPlayer extends React.Component {
   constructor(props){
     super(props);
     this.musicPlayer = React.createRef();
+    this.resize = this.resize.bind(this);
+
     this.timeLine = null;
     this.tween = new TweenMax("#audioPlayer-title", {paused: true, onStart: () => console.log("Tween go")}, {y: 200})
 
     this.state = {
-      clicked: false
+      clicked: false,
+      extended: false
+
     }
   }
+   resize(){
+     if (this.state.extended) {
+       this.musicPlayer.style.width = "unset";
+       this.setState({extended: false});
+     } else {
+       this.musicPlayer.style.width = "50em";
+      this.setState({extended: true});
+     }
+   }
+
 
    componentDidMount(){
    }
@@ -25,11 +39,11 @@ class SideBarAudioPlayer extends React.Component {
   return (
     <div id="SideBarAudioPlayer" style={{zIndex: 100}}
      ref={(div)=> this.musicPlayer = div}
-     onClick={() => this.animate()}
     className="elevatedCard">
         <div
           style={{padding:".5em", textAlign: "center"}}>
             <h5 id="audioPlayer-title">{"No Episode Selected"}</h5>
+            <button className="btn-outline-light" onClick={this.resize}> Resize </button>
         </div>
         <hr style={{marginBottom: ".1em", marginTop: ".1em"}}/>
         <div>
